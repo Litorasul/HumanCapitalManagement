41b03ec80b062f251d280201d42d15135677d6a3
Nikolay Kelov <n_kelov@yahoo.com>
2023-10-02 10:29:04 +0300

    Scaffolded Login, Logout, Register pages, Lock Register only for Admin, Add Role field in the Register process.

--- Client/HumanCapitalClient/HumanCapitalClient/Pages/Privacy.cshtml.cs
+++ Client/HumanCapitalClient/HumanCapitalClient/Pages/Privacy.cshtml.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HumanCapitalClient.Pages
 {
+    [Authorize(Roles="Manager")]
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
